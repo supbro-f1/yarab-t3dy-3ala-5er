d7f9bbcc5da4629308ac24d844dca57245863520
Maher <dodokandodo2016@gmail.com>
2026-05-01 20:58:58 +0300

    app

--- renovation/src/main/java/com/aastmt/renovation/RenovationApplication.java
+++ renovation/src/main/java/com/aastmt/renovation/RenovationApplication.java
@@ -1,1 +1,13 @@
-Hello world 
+package com.aastmt.renovation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenovationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RenovationApplication.class, args);
+	}
+
+}
