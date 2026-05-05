f60dc6f8750095c6cb7921db4a933f3adb25b3f5
YahyaKahilo <yahyakahilo@gmail.com>
2026-05-06 02:03:27 +0300

    Create UserRepository interface for user data access

---
+++ renovation/src/main/java/com/aastmt/renovation/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.aastmt.renovation.repository;
+
+import com.aastmt.renovation.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    
+    Optional<User> findByUsername(String username);
+
+}
