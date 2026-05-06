9439c30426c1ac68a381c9e420c4c76a5ded9e85
YahyaKahilo <yahyakahilo@gmail.com>
2026-05-06 15:57:50 +0300

    UserService imp

---
+++ renovation/src/main/java/com/aastmt/renovation/service/UserService.java
@@ -0,0 +1,61 @@
+import org.springframework.stereotype.Service;
+import com.aastmt.renovation.dto.UserLoginDto;
+import com.aastmt.renovation.dto.UserRegisterDto;
+import com.aastmt.renovation.model.User;
+import com.aastmt.renovation.repository.UserRepository;
+@Service
+public class UserService {
+    
+    private final UserRepository userrepo;
+
+    public UserService(UserRepository userrepo) {
+        this.userrepo = userrepo;
+    }
+
+    // GetUserProfile by id
+    public User getUserProfile(Long id) {
+        return userrepo.findById(id).orElseThrow(() -> new RuntimeException("User not found with id: " + id));
+    }
+    
+    // update user prof
+    public boolean updateUserProfile(Long id, UserRegisterDto dtoObj) {
+        
+        // user exists?
+        User existing = userrepo.findById(id).orElse(null);
+        if (existing == null) return false; 
+
+        //  new username is already taken?
+        if (userrepo.existsByUsername(dtoObj.getUsername()) 
+            && !existing.getUsername().equals(dtoObj.getUsername())) 
+            
+        //  email is already used?
+        if (userrepo.existsByEmail(dtoObj.getEmail()) 
+            && !existing.getEmail().equals(dtoObj.getEmail()))
+        return false;
+
+        // phone no. is already used?
+        if (userrepo.existsByPhoneNumber(dtoObj.getPhonenumber())
+            && !existing.getPhoneNumber().equals(dtoObj.getPhonenumber())) 
+        return false;
+
+
+        // update the user data
+        existing.setFirstName(dtoObj.getFname());
+        existing.setLastName(dtoObj.getLname());
+        existing.setUsername(dtoObj.getUsername());
+        existing.setPassword(dtoObj.getPassword());
+        existing.setEmail(dtoObj.getEmail());
+        existing.setPhoneNumber(dtoObj.getPhonenumber());
+
+        userrepo.save(existing);
+        return true;
+    } 
+
+    public boolean deleteUser(Long id) {
+        if (!userrepo.existsById(id))
+            return false;
+
+        userrepo.deleteById(id);
+        return true;
+    }
+  
